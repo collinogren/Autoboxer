43b76bb6e0767bed2b9dd34b3f7cc6cdff7b07dc
Collin Ogren <collinogren@gmail.com>
2024-04-19 22:47:25 -0500

    Replaced the GUI with JavaFX.

--- src/main/java/ogren/collin/autoboxer/gui/GUIFXController.java
+++ src/main/java/ogren/collin/autoboxer/gui/GUIFXController.java
@@ -1,25 +1,39 @@
+/*
+    Autoboxer to make creating "boxes" for figure skating competitions easier.
+    Copyright (C) 2024 Collin Ogren
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ogren.collin.autoboxer.gui;
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import ogren.collin.autoboxer.control.MasterController;
 import ogren.collin.autoboxer.pdf.PDFManipulator;
 
 import java.io.File;
 import java.net.URL;
-import java.text.NumberFormat;
 import java.util.ResourceBundle;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
